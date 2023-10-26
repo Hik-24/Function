ac496547237ec2f5b126d73a9bf7e56d74a36bc0
Hik-24 <104675985+Hik-24@users.noreply.github.com>
2023-10-26 08:30:05 +0700

    Add files via upload

---
+++ maxof2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int maxof(int a, int b){
+    if(a > b){
+        return a;
+    }
+    else {
+        return b;
+    }
+}
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    cout << maxof(n,m) << endl;
+    return 0;
+}
